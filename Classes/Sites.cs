d91f338a74b0af1e6f94907c2fd7f52925375393
JoePug <joepug@gmail.com>
2025-06-03 23:01:29 -0400

    Create Sites.cs
    added the sites class.

---
+++ Classes/Sites.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnCallScheduler
+{
+    class Sites
+    {
+        //this class will interact with the Main form
+        //responsible for managing the current site and all it's data and feeds it to the main form
+        //main form should only get it's data in the form it needs it to be
+
+        private List<string> SiteName = new List<string>();
+    }
+}
