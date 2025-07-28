202fffd2b09fc1ec91812a0ff19d1949d4bcebb1
JoePug <joepug@gmail.com>
2025-07-27 22:49:19 -0400

    Classes to draw and Print
    Just stubbed in the classes to draw the on call log and print. Not attached to anything.

---
+++ Classes/DrawPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnCallScheduler
+{
+    public class DrawPage
+    {
+        //takes a Site class, uses it then draws the page with the information from Site
+        //do I want this class to send it to the printer or just return the image?
+
+        public void CreateOnCallLog(Sites site) //only gonna draw one page so no need to make it do anything else
+        {
+
+        }
+
+    }
+}
